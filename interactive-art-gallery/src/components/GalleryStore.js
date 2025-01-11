ff09c3190d5068fd1fd25a25104fb36c3bacef8f
Karl Tay <karltay52@gmail.com>
2025-01-11 22:04:08 +0000

    local storage added

--- interactive-art-gallery/src/components/GalleryStore.js
+++ interactive-art-gallery/src/components/GalleryStore.js
@@ -1,11 +1,26 @@
 import { create } from "zustand";
 
 const useGalleryStore = create((set) => ({
-  artworks: [],
-  favorites: [],
-  addArtwork: (artwork) => set((state) => ({ artworks: [...state.artworks, artwork] })),
-  addFavorite: (artwork) => set((state) => ({ favorites: [...state.favorites, artwork] })),
-  removeFavorite: (id) => set((state) => ({ favorites: state.favorites.filter((art) => art.id !== id) })),
+  artworks: JSON.parse(localStorage.getItem("artworks")) || [],
+  favorites: JSON.parse(localStorage.getItem("favorites")) || [],
+  addArtwork: (artwork) =>
+    set((state) => {
+      const updatedArtworks = [...state.artworks, artwork];
+      localStorage.setItem("artworks", JSON.stringify(updatedArtworks));
+      return { artworks: updatedArtworks };
+    }),
+  addFavorite: (artwork) =>
+    set((state) => {
+      const updatedFavorites = [...state.favorites, artwork];
+      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+      return { favorites: updatedFavorites };
+    }),
+  removeFavorite: (id) =>
+    set((state) => {
+      const updatedFavorites = state.favorites.filter((art) => art.id !== id);
+      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+      return { favorites: updatedFavorites };
+    }),
 }));
 
 export default useGalleryStore;
